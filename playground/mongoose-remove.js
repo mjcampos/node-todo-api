9eecfff2d42b76e6d69a2bc3f1303ba30e304782
Michael Campos <camposmj89@gmail.com>
2017-07-02 23:21:29 -0700

    Add DELETE 'todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,15 @@
+var {ObjectID} = require('mongodb');
+var {mongoose} = require('./../server/db/mongoose');
+var {Todo} = require('./../server/models/todo');
+var {User} = require('./../server/models/user');
+
+// Todo.remove
+// Todo.remove({}).then((res) => {
+// 	console.log(res);
+// });
+
+// Todo.findOneAndRemove
+
+Todo.findByIdAndRemove('5959dfd62f6e49066bc9efd1').then((todo) => {
+	console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+	res.redirect('/todos');
+})
 
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
@@ -24,10 +30,12 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
-		res.send({todos});
+		// res.send({todos});
+		res.render('index.hbs', {todos});
 	}, (e) => {
 		res.status(400).send(e);
 	});
+
 });
 
 // GET /todos/:id
@@ -49,8 +57,29 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-app.listen(8888, () => {
-	console.log("Starting server");
+// DELETE /todos/:id
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	};
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		};
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+var port = 8888;
+
+app.listen(port, () => {
+	console.log("Starting server at port " + port);
 });
 
 module.exports = {
