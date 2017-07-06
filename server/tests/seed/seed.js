f2b749535fdeec6750027601c25417f5b9d57a37
Michael Campos <camposmj89@gmail.com>
2017-07-05 18:17:04 -0700

    Add tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,55 @@
+var {ObjectID} = require('mongodb');
+var jwt = require('jsonwebtoken');
+var {Todo} = require('./../../models/todo');
+var {User} = require('./../../models/user');
+
+var userOneId = new ObjectID();
+var userTwoId = new ObjectID();
+
+var users = [{
+	_id: userOneId,
+	email: 'camposmj89@gmail.com',
+	password: 'asdfasdf',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+	}]
+}, {
+	_id: userTwoId,
+	email: 'dherrera@gmail.com',
+	password: 'asdfasdf2',
+}];
+
+var todos = [{
+	_id: new ObjectID(),
+	text: 'First test todo'
+}, {
+	_id: new ObjectID(),
+	text: 'Second test todo',
+	completed: true,
+	completedAt: 333
+}];
+
+var populateTodos = (done) => {
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos)
+	}).then(() => done());
+};
+
+var populateUser = (done) => {
+	User.remove({}).then(() => {
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
+
+		return Promise.all([userOne, userTwo])
+	}).then(() => {
+		done();
+	});
+};
+
+module.exports = {
+	todos,
+	populateTodos,
+	users,
+	populateUser
+};
